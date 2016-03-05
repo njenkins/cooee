0a710bdffcd9744b165837d1199e46e3e757073c
njenkins <web@nathanjenkins.com.au>
2016-03-05 17:42:37 +1100

    Only call flickr for top 5 images

--- ui/js/cooee.js
+++ ui/js/cooee.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
     //Unique concepts required to handle watson returning duplicates.
     var rank = 1;
     results.forEach(function(elem) {
-      $.get('http://localhost:3000/image/'+elem[0], function(result){
-        if(rank < 6){
-            $('.row').append('<div class="col-5"><img class="img-circle" src="'+result.path+'" alt="catholic church"/><span class="concept">'+elem[0]+'</span><span class="score">RANK '+rank+'</score></div>');
-            rank++;
-        }
-      });
+      if(rank < 6){
+        $.get('http://localhost:3000/image/'+elem[0], function(result){
+              $('.row').append('<div class="col-5"><img class="img-circle" src="'+result.path+'" alt="catholic church"/><span class="concept">'+elem[0]+'</span><span class="score">RANK '+rank+'</score></div>');
+        });
+        rank++;
+  
+
+      }
 
     });
 
