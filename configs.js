2d2bb79b2ee986c964a250ba4bd05fd513fa6a91
Nathan Jenkins <Jenkins.Nathan@abc.net.au>
2016-03-08 08:51:38 +1100

    Add configs skeleton

---
+++ configs.js
@@ -0,0 +1,13 @@
+var configs = {
+ conceptInsights : {
+   username : 'yourconceptinsightsusername',
+   password : 'yourconceptinsightspassword'
+ },
+ flickr : {
+   api_key : 'yourflickrapikey',
+   secret : 'yourflickrsecret' 
+ }
+}
+module.exports = {
+  configs : configs
+}
