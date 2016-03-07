a08cad3c1c696627b3cb7797ad510b59f21abb73
njenkins <web@nathanjenkins.com.au>
2016-03-07 16:59:21 +1100

    Rank no longer required

--- ui/controllers/mainController.js
+++ ui/controllers/mainController.js
@@ -3,23 +3,20 @@ app.controller("cooee", function($scope, $http) {
     getTopConcepts(10)
     /*
     Get top concepts from articles
-    @param articleCount int Number of articles to use for analysis
+    @param articleCount {int} Number of articles to use for analysis
     */
-
     function getTopConcepts(articleCount){
       $http.get('http://localhost:3000/'+articleCount).then(function(response){
-        var rank = 1;
         $scope.topResults = [];
         response.data.forEach(function(elem) {
-            var concept = {};
-            concept.label = elem[0];
-            $http.get('http://localhost:3000/image/'+elem[0]).then(function(result){
-              concept.image = result.data.path;
-              $scope.topResults.push(concept);
-            },function(e){
-                console.log('something went wrong');
-            });
-            rank++;
+          var concept = {};
+          concept.label = elem[0];
+          $http.get('http://localhost:3000/image/'+elem[0]).then(function(result){
+            concept.image = result.data.path;
+            $scope.topResults.push(concept);
+          },function(e){
+              console.log('something went wrong');
+          });
         });
       }, function(e){
         console.log('something went wrong');
